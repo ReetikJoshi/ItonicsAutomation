919791f504df9052f53e791f2b3e15c0f048e5a5
Reetik <joshireetik2013@gmail.com>
2021-07-25 13:00:19 +0545

    files deleted

--- src/test/java/cucumberOptions/AmazonTestRunner.java
+++
@@ -1,8 +0,0 @@
-package cucumberOptions;
-
-import io.cucumber.junit.CucumberOptions;
-import io.cucumber.testng.AbstractTestNGCucumberTests;
-
-@CucumberOptions(features = "src/test/java/features", glue = { "stepDefinitions" })
-public class AmazonTestRunner extends AbstractTestNGCucumberTests {
-}
